d2a9ddaf4f6b21c9e9d6577caaa49cfa6f479f5a
aes89 <adrienne.e.smith@outlook.com>
2021-05-11 17:11:55 +1000

    Add: Timesheets Routes Index page required

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :timesheets
+  # resources :timesheets
+  get "/timesheets", to: "timesheets#index", as: "timesheets"
+  post "/timesheets", to: "timesheets#create"
+  get "/timesheets/new", to: "timesheets#new", as: "new_timesheet"
+  get "/timesheet/:id/edit", to: "timesheets#edit", as: "edit_timesheet"
+  get "/timesheets/:id", to: "timesheets#show", as: "timesheet"
+  patch "/timesheets/:id", to: "timesheets#update"
+  delete "timesheets/:id", to: "timesheets#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
